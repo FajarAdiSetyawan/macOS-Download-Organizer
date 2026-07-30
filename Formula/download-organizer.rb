552c3f197066827c27dbaf4dac01536ecd5fa571
Fajar Adi Setyawan <fajaradisetyawan9@gmail.com>
2026-07-30 15:20:31 +0700

    Fix brew URL case + update SHA256 for v1.1.1 archive

--- Formula/download-organizer.rb
+++ Formula/download-organizer.rb
@@ -1,8 +1,8 @@
 class DownloadOrganizer < Formula
   desc "Automatically organize your Downloads folder by file type"
   homepage "https://github.com/fajaradisetyawan/download-organizer"
-  url "https://github.com/fajaradisetyawan/download-organizer/archive/refs/tags/v1.1.1.tar.gz"
-  sha256 "PLACEHOLDER"
+  url "https://github.com/FajarAdiSetyawan/macOS-Download-Organizer/archive/refs/tags/v1.1.1.tar.gz"
+  sha256 "085abcfaf128ed46b359c11bfa020cff16b6fe9b08a8ae82ae83ab2d4e4bcf6e"
   license "MIT"
   version "1.1.1"
 
